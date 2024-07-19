e70c37ffd90c59f76d8f6f538c9b65b74a9ec385
happydatablare <happyrozta943@gmail.com>
2024-07-19 10:53:06 +0530

    testing deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/job_Portal_Testing/',
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Add this line
+  },
 })
